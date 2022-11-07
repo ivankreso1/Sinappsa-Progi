fd9d9612f411e23c67c5c63feb12fb64e88d0d5a
Neymarko <marko.prosenjak5@gmail.com>
2022-11-07 11:03:50 +0100

    Napravljene izmjene u Login.js komponenti: korištenje useState hooka, kao početna vrijednost je dan objekt s dva property-a (email i password).Dodan onChange event u inputove te mu je pridjeljena handler funkcija za praćenje promjena u inputu (za sada samo ispisuje promjene u inputovima i ažurira trenutnu vrijednost uz pomoć useState hooka). Inputi također imaju name i value atribute kako bi handler funkcija znala koji input ju triggera (u kojem inputu nastaju promjene)

--- IzvorniKod/Frontend/login-feature/src/components/Login.js
+++ IzvorniKod/Frontend/login-feature/src/components/Login.js
@@ -1,6 +1,20 @@
-import React from "react";
+import React, {useState} from "react";
 
 export default function Login() {
+
+    const [info, setInfo] = useState({
+        email: "",
+        password: ""
+    })      //pocetne informacije 
+
+    function handleInfoChange(e) {      //
+
+        console.log("User input: " + info.email + " " + info.password)  
+        setInfo(prevInfo => {
+            return { ...prevInfo, [e.target.name] : e.target.value}     //posto ima vise inputova, treba ih se razlikovati po name-u => to je jedan od parametara koji je sacuvan u 
+                                                                        //eventu (e) - pojedinom inputu se pristupa s e.target.name
+        })      //funkcija mijenja podatke koji se nalaze u "info" - info se ne smije direktno mijenjati nego se treba zadati callback funkcija
+    }
 
     return (
         <div>
@@ -11,18 +25,25 @@ export default function Login() {
                         type="email"
                         placeholder="E-mail adresa"
                         required
+                        name = "email"      //definiran je name kako bi se moglo zvati koji input je triggerao event
+                        value = {info.email}
+                        onChange = {handleInfoChange}
                     >
                     </input>
                     <input
                         type="password"
                         placeholder="Lozinka"
-                        required>
+                        required
+                        name="password"       //definiran je name kako bi se moglo zvati koji input je triggerao event
+                        value = {info.password}
+                        onChange={handleInfoChange}>
 
                     </input>
                     <button
                     type = "submit"> Submit </button>
                 </div>
             </form>
+            {(info.email && info.password) && <p> Input info: {info.email + " " + info.password}</p>} 
         </div>
     )
 }
