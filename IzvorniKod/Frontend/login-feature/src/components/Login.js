c4ece990bffc9b7e22c72f9055180669ec4a6da1
Neymarko <marko.prosenjak5@gmail.com>
2022-11-07 14:44:09 +0100

    Napravljen novi file (tests.js), napravljene promjene u Login.js: dodana handler funkcija za onSubmit event. U njoj se provjerava postoji li korisnički račun čija e-mail adresa i lozinka odgovaraju onima koje je korisnik upisao. Rezultat korisnikova upisa se uspoređuje s računima definiranima u tests.js (polje (array) s objektima koji imaju svoj id, e-mail adresu i lozinku). Ukoliko upisani podaci odgovaraju podacima nekog od unaprijed definiranih računa, na zaslon se ispisuje poruka o uspješnoj prijavi (ako se podaci promijene u krive, te se ponovno submitaju, poruka nestaje). Gumb Submit postaje onemogućen (disabled) dok se ne unesu i lozinka i e-mail adresa.

--- IzvorniKod/Frontend/login-feature/src/components/Login.js
+++ IzvorniKod/Frontend/login-feature/src/components/Login.js
@@ -1,33 +1,51 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
+import tests from "../tests"
 
 export default function Login() {
 
+
+    const[database] = useState([])
     const [info, setInfo] = useState({
         email: "",
         password: ""
     })      //pocetne informacije 
 
+    const [submited, setSubmited] = useState(false)
+
     function handleInfoChange(e) {      //
 
-        console.log("User input: " + info.email + " " + info.password)  
+        //console.log("User input: " + info.email + " " + info.password)  
         setInfo(prevInfo => {
-            return { ...prevInfo, [e.target.name] : e.target.value}     //posto ima vise inputova, treba ih se razlikovati po name-u => to je jedan od parametara koji je sacuvan u 
-                                                                        //eventu (e) - pojedinom inputu se pristupa s e.target.name
+            return { ...prevInfo, [e.target.name]: e.target.value }     //posto ima vise inputova, treba ih se razlikovati po name-u => to je jedan od parametara koji je sacuvan u 
+            //eventu (e) - pojedinom inputu se pristupa s e.target.name
         })      //funkcija mijenja podatke koji se nalaze u "info" - info se ne smije direktno mijenjati nego se treba zadati callback funkcija
+    }
+
+    function handleSubmit(e) {  //poziva se kada se forma submita (pritiskom na submit gumb)
+        e.preventDefault()  //zaustavlja se defaultno ponasanje weba da se stranica refresha - ja sam htio ispisati podatke i ostaviti podatke u input poljima
+        setSubmited(false)      //cim se nesto mijenjalo (lozinka ili e-mail adresa), mice se poruka o uspjesnom loginu - inace bi stalno bilo true i 
+                                //stalno bi se ispisivala poruka o uspjesnoj prijavi
+        tests.map(test => {
+            if (test.email === info.email 
+                &&
+                test.password === info.password) {
+                    setSubmited(true)
+                }
+        })
     }
 
     return (
         <div>
             <h1>Login</h1>
-            <form>
+            <form onSubmit={handleSubmit}>
                 <div className="info-input">
                     <input
                         type="email"
                         placeholder="E-mail adresa"
                         required
-                        name = "email"      //definiran je name kako bi se moglo zvati koji input je triggerao event
-                        value = {info.email}
-                        onChange = {handleInfoChange}
+                        name="email"      //definiran je name kako bi se moglo zvati koji input je triggerao event
+                        value={info.email}
+                        onChange={handleInfoChange}
                     >
                     </input>
                     <input
@@ -35,15 +53,16 @@ export default function Login() {
                         placeholder="Lozinka"
                         required
                         name="password"       //definiran je name kako bi se moglo zvati koji input je triggerao event
-                        value = {info.password}
+                        value={info.password}
                         onChange={handleInfoChange}>
 
                     </input>
                     <button
-                    type = "submit"> Submit </button>
+                        type="submit"
+                        disabled={!info.email || !info.password ? true : false}> Submit </button>
                 </div>
             </form>
-            {(info.email && info.password) && <p> Input info: {info.email + " " + info.password}</p>} 
+            {submited && <p> Uspješno ste se ulogirali </p>}
         </div>
     )
 }
---
+++ IzvorniKod/Frontend/login-feature/src/tests.js
@@ -0,0 +1,24 @@
+export default [
+    {
+        id: 1,
+        email: "pero.peric@gmail.com",
+        password: "perinaSifra"
+    },
+
+    {
+        id: 2,
+        email: "grga.grgic@gmail.com",
+        password: "grginaSifra"
+    },
+
+    {
+        id: 3,
+        email: "ivan.ivic@gmail.com",
+        password: "ivanovaSifra"
+    },
+    {
+        id: 4,
+        email: "luka.lukic@gmail.com",
+        password: "lukinaSifra"
+    }
+]
