bea743fb14819776be01c82921bec7db0b8e5d36
ma_tea <mk53416@fer.hr>
2022-12-16 01:04:56 +0100

    Dohvat kolegija i prikaz u formi, prikaz kategorija u formi.

--- IzvorniKod/frontend/src/components/CreateAd.js
+++ IzvorniKod/frontend/src/components/CreateAd.js
@@ -1,30 +1,44 @@
 import { Button, Card, DropdownButton, Form } from "react-bootstrap";
 import DropdownItem from "react-bootstrap/esm/DropdownItem";
+import React, { useState } from "react";
+import configData from "../resources/config.json";
 
 
 export default function CreateAd() {
+
+   const [kolegiji, setKolegiji] = useState([""])
+   let categories = ["LABOS", "BLIC", "GRADIVO", "KONTINUIRANI_ISPIT", "ISPITNI_ROK"]
+
+   React.useEffect( () => {
+      fetch(`${configData.hostname}/kolegiji`)
+         .then(res => res.json()
+         .then(data => {
+         setKolegiji(data)
+      }))
+   }, [])
+      
+   function onSubmit(event) {
+      event.preventDefault();
+   }
+
    return (
       <><h1> Stranica za kreiranje oglasa </h1>
       <div className="container">
          <Card style={{boxShadow: "rgba(0, 0, 0, 0.3) 0px 19px 38px, rgba(0, 0, 0, 0.22) 0px 15px 12px", minWidth: "400px", maxWidth: "480px", display: "flex", alignContent: "center", alignItems: "center", borderRadius: "10px"}}>
-            <Form className="form">
+            <Form className="form" onSubmit={onSubmit}>
                <Form.Group>
-                  <Form.Label>Naslov</Form.Label>
-                  <Form.Control type="text" placeholder="Naslov"></Form.Control>
+                  <Form.Label>Naslov oglasa</Form.Label>
+                  <Form.Control type="text" placeholder="Naslov" name="naslov"></Form.Control>
                </Form.Group>
                <Form.Group>
-                  <Form.Label>Opis</Form.Label>
-                  <Form.Control as="textarea" rows="3"></Form.Control>
+                  <Form.Label>Opis problema/ponude</Form.Label>
+                  <Form.Control as="textarea" rows="3" name="opis"></Form.Control>
                </Form.Group>
-               <DropdownButton>
-                  <DropdownItem>Prvi</DropdownItem>
-                  <DropdownItem>Drugi</DropdownItem>
-                  <DropdownItem>Treći</DropdownItem>
+               <DropdownButton name="kolegij" title='Kolegiji'>
+                  {kolegiji.map(kolegij => {return <DropdownItem name="kolegij">{kolegij.ime}</DropdownItem>})}
                </DropdownButton>
-               <DropdownButton>
-                  <DropdownItem>1.</DropdownItem>
-                  <DropdownItem>2.</DropdownItem>
-                  <DropdownItem>3.</DropdownItem>
+               <DropdownButton name="kategorija" title='Kategorija'>
+                  {categories.map(cat => {return <DropdownItem name="kategorija">{cat}</DropdownItem>})}
                </DropdownButton>
                <Button type="submit">Submit</Button>
             </Form>
