cd5240cfb362955b14ea5f5a62e24f3197f7473f
ma_tea <mk53416@fer.hr>
2022-12-17 20:01:39 +0100

    Dodana navigacijska traka, dodana provjera jesu li svi podaci uneseni, dodan RESET gumb, poboljšana forma

--- IzvorniKod/frontend/src/components/CreateAd.js
+++ IzvorniKod/frontend/src/components/CreateAd.js
@@ -1,9 +1,22 @@
-import { Button, Card, DropdownButton, Form } from "react-bootstrap";
+import {
+  Button,
+  ButtonGroup,
+  Card,
+  DropdownButton,
+  Form,
+} from "react-bootstrap";
 import DropdownItem from "react-bootstrap/esm/DropdownItem";
 import React, { useState } from "react";
 import configData from "../resources/config.json";
+import Navbar from "./home/Navbar";
+import { getPersonInfo } from "../scripts/util";
 
 export default function CreateAd() {
+  const formStyle = {
+    margin: "auto",
+    marginBottom: "1%",
+  };
+
   const [ad, setAd] = useState({
     radnja: "",
     kolegij: "",
@@ -13,7 +26,7 @@ export default function CreateAd() {
   });
 
   const [kolegiji, setKolegiji] = useState([""]);
-  let categories = [
+  let kategorije = [
     "LABOS",
     "BLIC",
     "GRADIVO",
@@ -29,12 +42,14 @@ export default function CreateAd() {
     );
   }, []);
 
-  function onSubmit(event) {
-    event.preventDefault();
-
+  function handleAdChange(event) {
     setAd((prevAd) => {
       return { ...prevAd, [event.target.name]: event.target.value }; //posto ima vise inputova, treba ih se razlikovati po name-u => to je jedan od parametara koji je sacuvan u event.target
     });
+  }
+
+  function onSubmit(event) {
+    event.preventDefault();
 
     const data = {
       radnja: ad.radnja,
@@ -48,74 +63,111 @@ export default function CreateAd() {
   }
 
   return (
-    <div className="container">
-      <Card
-        style={{
-          boxShadow:
-            "rgba(0, 0, 0, 0.3) 0px 19px 38px, rgba(0, 0, 0, 0.22) 0px 15px 12px",
-          minWidth: "400px",
-          maxWidth: "480px",
-          display: "flex",
-          alignContent: "center",
-          alignItems: "center",
-          borderRadius: "10px",
-        }}
-      >
-        <h1> Kreiraj novi oglas! </h1>
-        <Form className="form" onSubmit={onSubmit}>
-          {["radio"].map((type) => (
-            <div key={`inline-${type}`} className="mb-3">
+    <div>
+      <Navbar getPersonInfo={getPersonInfo}></Navbar>
+
+      <div className="container">
+        <Card
+          style={{
+            boxShadow:
+              "rgba(0, 0, 0, 0.3) 0px 19px 38px, rgba(0, 0, 0, 0.22) 0px 15px 12px",
+            minWidth: "400px",
+            maxWidth: "480px",
+            display: "flex",
+            alignContent: "center",
+            alignItems: "center",
+            borderRadius: "10px",
+          }}
+        >
+          <h1> Kreiraj novi oglas! </h1>
+          <Form className="form" onSubmit={onSubmit}>
+            <div className="mb-3">
               <Form.Check
                 inline
                 label="Tražim pomoć!"
                 name="radnja"
-                type={type}
-                id={`inline-${type}-1`}
+                type="radio"
+                value="true"
+                onChange={handleAdChange}
               />
               <Form.Check
                 inline
                 label="Nudim pomoć!"
                 name="radnja"
-                type={type}
-                id={`inline-${type}-2`}
+                type="radio"
+                value="false"
+                onChange={handleAdChange}
               />
             </div>
-          ))}
-          <div className="mb-3">
-            <DropdownButton name="kolegij" title="Kolegij">
-              {kolegiji.map((kolegij) => {
-                return (
-                  <DropdownItem name="kolegij">{kolegij.ime}</DropdownItem>
-                );
-              })}
-            </DropdownButton>
-          </div>
-          <div className="mb-3">
-            <DropdownButton name="kategorija" title="Kategorija">
-              {categories.map((cat) => {
-                return <DropdownItem name="kategorija">{cat}</DropdownItem>;
-              })}
-            </DropdownButton>
-          </div>
 
-          <div className="mb-3">
-            <Form.Label>Naslov oglasa</Form.Label>
-            <Form.Control
-              type="text"
-              placeholder="Naslov"
-              name="naslov"
-            ></Form.Control>
-          </div>
+            <div className="mb-3">
+              <Form.Select
+                aria-label="Default select example"
+                name="kolegij"
+                onChange={handleAdChange}
+              >
+                <option value="">Kolegij</option>
+                {kolegiji.map((kolegij) => {
+                  return <option value={kolegij.ime}>{kolegij.ime}</option>;
+                })}
+              </Form.Select>
+            </div>
 
-          <div className="mb-3">
-            <Form.Label>Opis problema/ponude</Form.Label>
-            <Form.Control as="textarea" rows="3" name="opis"></Form.Control>
-          </div>
-          <div className="mb-3">
-            <Button type="submit">Podnesi</Button>
-          </div>
-        </Form>
-      </Card>
+            <div className="mb-3">
+              <Form.Select
+                aria-label="Default select example"
+                name="kategorija"
+                onChange={handleAdChange}
+              >
+                <option value="">Kategorija</option>
+                {kategorije.map((kategorija) => {
+                  return <option value={kategorija}>{kategorija}</option>;
+                })}
+              </Form.Select>
+            </div>
+
+            <div className="mb-3">
+              <Form.Label>Naslov oglasa</Form.Label>
+              <Form.Control
+                type="text"
+                placeholder="Naslov"
+                name="naslov"
+                onChange={handleAdChange}
+              ></Form.Control>
+            </div>
+
+            <div className="mb-3">
+              <Form.Label>Opis problema/ponude</Form.Label>
+              <Form.Control
+                as="textarea"
+                rows="3"
+                name="opis"
+                onChange={handleAdChange}
+              ></Form.Control>
+            </div>
+            <ButtonGroup className="mb-3 d-flex flex-row">
+              <Button type="reset" variant="danger">
+                Očisti
+              </Button>
+              <Button
+                style={{ marginRight: "auto" }}
+                type="submit"
+                disabled={
+                  !ad.radnja ||
+                  !ad.kolegij ||
+                  !ad.kategorija ||
+                  !ad.naslov ||
+                  !ad.opis
+                    ? true
+                    : false
+                }
+              >
+                Podnesi
+              </Button>
+            </ButtonGroup>
+          </Form>
+        </Card>
+      </div>
     </div>
   );
 }
