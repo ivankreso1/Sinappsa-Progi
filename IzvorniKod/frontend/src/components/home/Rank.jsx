82447dcf794d2042bfad53d6354124bfae2e81d2
ma_tea <mk53416@fer.hr>
2022-12-20 17:57:07 +0100

    Uređena rejting lista.

--- IzvorniKod/frontend/src/components/home/Rank.jsx
+++ IzvorniKod/frontend/src/components/home/Rank.jsx
@@ -1,19 +1,16 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 import "../../cssFiles/home/rank.css";
 
-
 class Rank extends Component {
-
-    render() { 
-        return (
-            <div className="rank-container">
-                <h5 className="rank-item rank-mark">{this.props.rank}#</h5>
-                <h6 className="rank-item">{this.props.user.korisnickoIme}</h6>
-                <h5 className="rank-item">{this.props.user.prosjek}</h5>
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div className="rank-container">
+        <h5 className="rank-item rank-mark">{this.props.rank}#</h5>
+        <h6 className="rank-item">{this.props.user.korisnickoIme}</h6>
+        <h5 className="rank-item">{this.props.user.prosjek.toFixed(2)}</h5>
+      </div>
+    );
+  }
 }
- 
 
 export default Rank;
