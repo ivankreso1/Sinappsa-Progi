d846ff91ee39f7356bc833d85c02bb9c086daf81
Neymarko <marko.prosenjak5@gmail.com>
2022-12-20 17:18:23 +0100

    Dodani avatari, promijenjen stil kako bi stali u card

--- IzvorniKod/frontend/src/avatars.js
+++ IzvorniKod/frontend/src/avatars.js
@@ -1,6 +1,12 @@
 import Avatar1 from "./avatars/Avatar1.png"
 import Avatar2 from "./avatars/Avatar2.png"
 import Avatar3 from "./avatars/Avatar3.png"
+import Avatar4 from "./avatars/Avatar4.png"
+import Avatar5 from "./avatars/Avatar5.png"
+import Avatar6 from "./avatars/Avatar6.png"
+import Avatar7 from "./avatars/Avatar7.png"
+import Avatar8 from "./avatars/Avatar8.png"
+import Avatar9 from "./avatars/Avatar9.png"
 
 let avatars =  [
     {
@@ -10,10 +16,41 @@ let avatars =  [
     {
         id: "2",
         src: Avatar2
-    },
+    }
+    ,
     {
         id: "3",
         src: Avatar3
+    },
+
+    {
+        id: "4",
+        src: Avatar4
+    }
+    ,
+    {
+        id: "5",
+        src: Avatar5
+    }
+    ,
+    {
+        id: "6",
+        src: Avatar6
+    }
+    ,
+    {
+        id: "7",
+        src: Avatar7
+    }
+    ,
+    {
+        id: "8",
+        src: Avatar8
+    }
+    ,
+    {
+        id: "9",
+        src: Avatar9
     }
 ]
 
