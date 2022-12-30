42af95b91f5ce267662d8f5f8f1b6bce9d56a308
lovrog92 <lovro.gacina@fer.hr>
2022-12-30 01:17:34 +0100

    Dodana prva verzija prikaza upita na profilu

---
+++ IzvorniKod/frontend/src/components/profile/UserQueries.jsx
@@ -0,0 +1,46 @@
+import React, { Component } from "react";
+import { getDataAuth, getPersonInfo } from "../../scripts/util";
+import Ad from "../shared/Ad"
+import Query from "./Query";
+import "../../cssFiles/Profile.css";
+import "../../cssFiles/shared/shared.css";
+
+class UserQueries extends Component {
+	state = {
+		userQueries: []
+	};
+
+	componentDidMount() {
+		this.handleUserQueries()
+
+	};
+
+	handleUserQueries = () => {
+		const personInfo = getPersonInfo();
+
+		getDataAuth("upiti/autorUpita/" + personInfo.id).then((data) =>
+			this.setState({ userQueries: data })
+		);
+		console.log(this.state.userQueries ? "Pun" : "Prazan");
+	}
+
+	render() {
+		return (
+			<React.Fragment>
+				<div className="profile-section">
+					<h2 className="section-title section-title-secondary-color"> Moji upiti </h2>
+					{
+					this.state.userQueries.map(query =>
+                        	<Query 
+                            	key={`query${this.state.userQueries.indexOf(query)}`} 
+                            	query={query}
+                        	/>
+                    )
+					}
+				</div>
+			</React.Fragment>
+		);
+	}
+}
+
+export default UserQueries;
