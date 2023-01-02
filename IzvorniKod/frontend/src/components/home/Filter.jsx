ff5ae592d6b2a1af00f7bd92016f339b94d9b939
Karlito16 <karlo.dimjasevic@gmail.com>
2023-01-02 17:29:43 +0100

    Refaktoriran cijeli filter bar. Ispravljen prikaz kolegija za filtrirani smjer. Ispravljena greška beskonačnog renderiranja. 'Real-time' filtriranje napravljeno na efikasniji način.

--- IzvorniKod/frontend/src/components/home/Filter.jsx
+++ IzvorniKod/frontend/src/components/home/Filter.jsx
@@ -1,33 +1,75 @@
 import React, { Component } from 'react';
-// import { Form, Button } from "react-bootstrap";
-// import DropdownButton from 'react-bootstrap/DropdownButton';
-// import DropdownItem from 'react-bootstrap/esm/DropdownItem';
 import "../../cssFiles/home/filter.css";
 import "bootstrap";
+import { getData } from '../../scripts/util';
 
 
 class Filter extends Component {
     
-    state = {
-        categories: [
-            "Labos", 
-            "Blic", 
-            "Gradivo", 
-            "Kontinuirani ispit", 
-            "Ispitni rok"
-        ]
-    };
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            direction: "",
+            course: "",
+            category: "",
+            courses: [],
+            categories: [
+                "Labos", 
+                "Blic", 
+                "Gradivo", 
+                "Kontinuirani ispit", 
+                "Ispitni rok"
+            ]
+        };
+
+        this.fetchCourses();
+    }
+
+    componentDidMount() {
+        this.fetchAds();
+    }
+
+    handleDropDownClick = (e) => {
+        this.setState(
+            { [e.target.name]: e.target.value },
+            () => this.afterDropDownClick(),
+        );
+    }
+
+    afterDropDownClick = () => {
+        if (this.state.direction !== "") {
+            if (!this.state.courses
+                .filter(course => course.smjer === this.state.direction)
+                .map(course => course.ime)
+                .includes(this.state.course)) {
+                this.state.course = "";
+            }
+        }
+
+        this.fetchAds();
+    }
+
+    fetchAds = () => {
+        getData(`/oglasi/filter?smjer=${this.state.direction}&kategorija=${this.state.category}&kolegij=${this.state.course.replace(/ /g, "+")}`)
+        .then(data => this.props.onFilter(data));
+    }
+
+    fetchCourses = () => {
+        let path = this.state.direction === "" ? "" : `/smjer/${this.state.direction}`;
+
+        getData(`/kolegiji${path}`)
+        .then(data => this.setState({ courses: data }));
+    }
 
     render() { 
-        const formInfo = this.props.formInfo;
-
         return (
             <div className="row">
                 <div className="col-lg-15 mb-3">
                     <div className="row text-left mb-2">
                         <div className="col-lg-3 mb-3 mb-sm-0">
                             <div className="dropdown bootstrap-select form-control form-control-lg bg-white bg-op-9 text-sm w-lg-50" style={{width: "100%"}}>
-                                <select name="smjer" className="form-control form-control-lg bg-white bg-op-9 text-sm w-lg-50" data-toggle="select" tabindex="-98" onChange={this.props.onDropDownClick}>
+                                <select name="direction" className="form-control form-control-lg bg-white bg-op-9 text-sm w-lg-50" data-toggle="select" tabindex="-98" onChange={this.handleDropDownClick}>
                                     <option value="">Svi smjerovi</option>
                                     <option value="R">R smjer</option>
                                     <option value="E">E smjer</option>
@@ -36,17 +78,19 @@ class Filter extends Component {
                         </div>
                         <div className="col-lg-5 mb-3 mb-sm-0">
                             <div className="dropdown bootstrap-select form-control form-control-lg bg-white bg-op-9 text-sm w-lg-50" style={{width: "100%"}}>
-                                <select name="kolegij" className="form-control form-control-lg bg-white bg-op-9 text-sm w-lg-50" data-toggle="select" tabindex="-98" onChange={this.props.onDropDownClick}>
+                                <select name="course" className="form-control form-control-lg bg-white bg-op-9 text-sm w-lg-50" data-toggle="select" tabindex="-98" onChange={this.handleDropDownClick}>
                                     <option value="">Svi kolegiji</option>
-                                    {this.props.kolegiji.map(kolegij => {
-                                        return <option value={kolegij.ime}>{kolegij.ime}</option>
+                                    {this.state.courses.map(course => {
+                                        if (this.state.direction === "" || course.smjer === this.state.direction)
+                                            return <option value={course.ime}>{course.ime}</option>
+                                        return "";
                                     })}
                                 </select>
                             </div>
                         </div>
                         <div className="col-lg-4 mb-3 mb-sm-0">
                             <div className="dropdown bootstrap-select form-control form-control-lg bg-white bg-op-9 text-sm w-lg-50" style={{width: "100%"}}>
-                                <select name="kategorija" className="form-control form-control-lg bg-white bg-op-9 text-sm w-lg-50" data-toggle="select" tabindex="-98" onChange={this.props.onDropDownClick}>
+                                <select name="category" className="form-control form-control-lg bg-white bg-op-9 text-sm w-lg-50" data-toggle="select" tabindex="-98" onChange={this.handleDropDownClick}>
                                     <option value="">Sve kategorije</option>
                                     {this.state.categories.map(category => {
                                         return <option value={category.toUpperCase().replace(" ", "_")}>{category}</option>
--- IzvorniKod/frontend/src/components/home/Home.js
+++ IzvorniKod/frontend/src/components/home/Home.js
@@ -1,154 +1,52 @@
-import React, { useState, useEffect } from "react";
-import configData from "../../resources/config.json";
-import "../../cssFiles/home/home.css";
-import "../../cssFiles/shared/shared.css";
+import React, { Component } from 'react';
 import Filter from "./Filter";
 import AdList from "../shared/AdList";
 import RankList from "./RankList";
 import Navbar from "./Navbar";
 import AddCourse from "./AddCourse";
-import { getPersonInfo } from "../../scripts/util";
 import CreateAd from "./CreateAd";
+import "../../cssFiles/home/home.css";
+import "../../cssFiles/shared/shared.css";
+import { getPersonInfo } from '../../scripts/util';
 
-function App() {
-  const [kolegiji, setKolegiji] = useState([""]);
-  const [oglasi, setOglasi] = useState([]);
-  const [checkedRadioButton, setCheckedRadioButton] = useState("");
-  const [formInfo, setFormInfo] = useState({
-    smjer: "",
-    kolegij: "",
-    kategorija: "",
-  });
+class Home extends Component {
+  
+  constructor(props) {
+    super(props);
 
-  function fetchNotFiltered() {
-    fetch(`${configData.hostname}/kolegiji`).then((res) =>
-      res.json().then((data) => {
-        // console.log(data)
-        setKolegiji(data);
-      })
-    );
-    fetch(`${configData.hostname}/oglasi/filter?smjer=&kategorija=&kolegij=`)
-      .then((res) => res.json())
-      .then((data) => {
-        // console.log(data)
-        setOglasi(data);
-      });
-  } /*ovaj dio je izdvojen u funkciju jer se koristi u useeffectu i kada se očiste opcije filtriranja (resetFilterOption)*/
-
-  useEffect(() => {
-    fetchNotFiltered();
-  }, []);
-
-  useEffect(() => {
-    fetch(
-      `${configData.hostname}/oglasi/filter?smjer=${
-        formInfo.smjer
-      }&kategorija=${formInfo.kategorija}&kolegij=${formInfo.kolegij.replace(
-        / /g,
-        "+"
-      )}`
-    )
-      .then((res) => res.json())
-      .then((data) => {
-        // console.log(data)
-        setOglasi(data);
-      });
-  }, [oglasi]); //SLUZI tome da se stranica refresha u trenutku kada se obrise neki od oglasa
-
-  //u ovoj funkciji treba fetchati sve kolegije s backenda koji postoje u bazi podataka
-  function dohvatiKolegije(smjer) {
-    //console.log(smjer)
-    fetch(`${configData.hostname}/kolegiji/smjer/${smjer.toLowerCase()}`)
-      .then((res) => res.json())
-      .then((data) => {
-        // console.log(data)
-        setTimeout(() => {
-          setKolegiji(data);
-        }, 10); //riješen problem višestrukog renderanja
-        // console.log(kolegiji)
-      });
+    this.state = {
+      ads: []
+    }
   }
 
-  function handleFormSubmit(e) {
-    e.preventDefault();
-    // console.log(formInfo)       //target je cijela forma koja se submita
-    fetch(
-      `${configData.hostname}/oglasi/filter?smjer=${
-        formInfo.smjer
-      }&kategorija=${formInfo.kategorija}&kolegij=${formInfo.kolegij.replace(
-        / /g,
-        "+"
-      )}`
-    )
-      .then((res) => res.json())
-      .then((data) => {
-        // console.log(data)
-        setOglasi(data);
-      });
+  handleFilter = (ads) => {
+    this.setState({ ads: ads });
   }
 
-  function optionDropDownClick(e) {
-    setFormInfo((prevInfo) => {
-      return { ...prevInfo, [e.target.name]: e.target.value };
-    });
-  }
-
-  function changeFormInfo(e) {
-    /*ovdje se trebaju fetchati svi kolegiji za određen smjer*/
-    console.log(e.target.value);
-    dohvatiKolegije(e.target.value);
-    setFormInfo((prevInfo) => {
-      return { ...prevInfo, [e.target.name]: e.target.value };
-    });
-    setCheckedRadioButton(e.target.value);
-  }
-
-  function resetFilterOption() {
-    setFormInfo({
-      smjer: "",
-      kolegij: "",
-      kategorija: "",
-    });
-    //fetchNotFiltered();
-
-    fetch(`${configData.hostname}/kolegiji`).then((res) =>
-      res.json().then((data) => {
-        // console.log(data)
-        setKolegiji(data);
-      })
-    );
-
-    setCheckedRadioButton("");
-  }
-
-  function mapAds(ads) {
+  mapAds = (ads) => {
     return ads.map((ad) => Object({ oglas: ad, listaUpita: [] }));
   }
 
-  return (
-    <div className="home-page">
-      <Navbar getPersonInfo={getPersonInfo}></Navbar>
-      <div className="body-wrapper">
-        <div className="body-wrapper-child child1">
-          <Filter
-            key="filter"
-            kolegiji={kolegiji}
-            formInfo={formInfo}
-            checkedRadioButton={checkedRadioButton}
-            resetFilterOption={resetFilterOption}
-            onFormSubmit={(e) => handleFormSubmit(e)}
-            onFormInfo={(e) => changeFormInfo(e)}
-            onDropDownClick={(e) => optionDropDownClick(e)}
-          />
-          <AdList key="adList" data={mapAds(oglasi)} forProfile={false} />
-        </div>
-        <div className="body-wrapper-child">
-          {getPersonInfo().isModerator ? <AddCourse /> : <CreateAd />}
-          <RankList key="rankList" />
+  render() { 
+    return (
+      <div className="home-page">
+        <Navbar getPersonInfo={getPersonInfo}></Navbar>
+        <div className="body-wrapper">
+          <div className="body-wrapper-child child1">
+            <Filter
+              key="filter"
+              onFilter={(ads) => this.handleFilter(ads)}
+            />
+            <AdList key="adList" data={this.mapAds(this.state.ads)} forProfile={false} />
+          </div>
+          <div className="body-wrapper-child">
+            {getPersonInfo().isModerator ? <AddCourse /> : <CreateAd />}
+            <RankList key="rankList" />
+          </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
-
-export default App;
+ 
+export default Home;
