dbdb81122aa4e6f9c87551271337e86371f01838
ma_tea <mk53416@fer.hr>
2023-01-11 17:21:39 +0100

    Dodan test za registraciju.

--- IzvorniKod/frontend/test/test.js
+++ IzvorniKod/frontend/test/test.js
@@ -6,31 +6,58 @@ const loginUrl = rootUrl + "/login";
 const registerUrl = rootUrl + "/register";
 
 describe("React Sinappsa Frontend Registration Test", () => {
-	var driver;
+  var driver;
 
-	before(() => {
-		driver = new webdriver.Builder().forBrowser("firefox").build();
-	});
+  before(() => {
+    driver = new webdriver.Builder().forBrowser("firefox").build();
+  });
 
-	it("go to login", () => {
-		driver.get(rootUrl).then(() => {
-			driver.findElement(webdriver.By.linkText("Prijava"))
-				.then((element) => element.click())
-				.then(() => driver.getCurrentUrl())
-				.then((url) => assert.equal(url, loginUrl))
-			    .catch(() => assert.fail("Test failed"));
-		});
-	});
+  it("go to login", () => {
+    driver.get(rootUrl).then(() => {
+      driver
+        .findElement(webdriver.By.linkText("Prijava"))
+        .then((element) => element.click())
+        .then(() => driver.getCurrentUrl())
+        .then((url) => assert.equal(url, loginUrl))
+        .catch(() => assert.fail("Test failed"));
+    });
+  });
 
-    it("go to register", () => {
-        driver.get(loginUrl).then(() => {
-            driver.findElement(webdriver.By.linkText("Registriraj se ovdje"))
-            .then((element) => element.click())
-            .then(() => driver.getCurrentUrl())
-            .then((url) => assert.equal(url, registerUrl))
-            .catch(() => assert.fail("Test failed"));
-        })
-    })
+  it("go to register", () => {
+    driver.get(loginUrl).then(() => {
+      driver
+        .findElement(webdriver.By.linkText("Registriraj se ovdje"))
+        .then((element) => element.click())
+        .then(() => driver.getCurrentUrl())
+        .then((url) => assert.equal(url, registerUrl))
+        .catch(() => assert.fail("Test failed"));
+    });
+  });
 
-	after(() => driver.quit());
+  it("name input", () => {
+    driver.get(registerUrl).then(() => {
+      driver
+        .findElement(webdriver.By.id("ime"))
+        .sendKeys("Testno Ime\n")
+        .then(() => {
+          driver.getPageSource().then((source) => {
+            assert.equal(source.includes("Testno Ime"), true);
+          });
+        });
+    });
+  });
+  it("surname input", () => {
+    driver.get(registerUrl).then(() => {
+      driver
+        .findElement(webdriver.By.id("prezime"))
+        .sendKeys("Testno Prezime\n")
+        .then(() => {
+          driver.getPageSource().then((source) => {
+            assert.equal(source.includes("Testno Prezime"), true);
+          });
+        });
+    });
+  });
+
+  after(() => driver.quit());
 });
